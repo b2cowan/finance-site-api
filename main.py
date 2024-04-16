81fd2f11b18542c46baf7d43a6bb13ac6aca2926
b2cowan <55103499+b2cowan@users.noreply.github.com>
2024-04-15 23:12:41 -0400

    renamed:    application.py -> main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from tickers import test_response
 
-application = Flask(__name__)
-cors = CORS(application, origins='*')
+app = Flask(__name__)
+cors = CORS(app, origins='*')
 
-@application.route("/api/stocks/<ticker>", methods=['GET'])
+@app.route("/api/stocks/<ticker>", methods=['GET'])
 def stocks(ticker):
     stock_list = test_response(ticker) 
     return stock_list
 
 if __name__ == "__main__":
-    application.run(debug=True, port=8080)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
